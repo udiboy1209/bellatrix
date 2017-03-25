9d994a89239c5ff28695f21e743feae50db9aa7f
Kumar Ayush <cheekujodhpur@gmail.com>
2017-03-25 15:27:32 +0530

    calibration fairly done

--- capture.py
+++ capture.py
@@ -6,8 +6,6 @@ import pygame
 
 cap = cv2.VideoCapture(0)
 
-cv2.namedWindow('video')
-cv2.namedWindow('cropped')
 FINAL_MIN = np.array([0, 210, 0])
 FINAL_MAX = np.array([255, 255, 255])
 
@@ -19,8 +17,14 @@ FINAL_MAX = np.array([255, 255, 255])
 
 # pygame.display.flip()
 
-while True:
-    ret, frame = cap.read()
+MEAN_BOUNDARY_SIZE = 100.0
+sum_boundary = []
+mean_boundary = None
+sum_boundary_count = 0
+
+def calibrate_screen(frame):
+
+    global mean_boundary,MEAN_BOUNDARY_SIZE,sum_boundary,sum_boundary_count
 
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -36,27 +40,52 @@ while True:
     screenRect = None
     for contour in contours:
         peri = cv2.arcLength(contour, True)
-	approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
+        approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
 
-	# if our approximated contour has four points, then
-	# we can assume that we have found our screen
-	if len(approx) == 4:
-		screenRect = approx
-		break
+        # if our approximated contour has four points, then
+        # we can assume that we have found our screen
+        if len(approx) == 4:
+                screenRect = approx
+                break
 
-#     print(screenRect[0][0])
-#     if screenRect:
-#         minX = min(screenRect[0][0][0],screenRect[1][0][0])
-#         maxX = min(screenRect[2][0][0],screenRect[3][0][0])
-#         minY = max(screenRect[0][0][1],screenRect[3][0][1])
-#         maxY = max(screenRect[2][0][1],screenRect[1][0][1])
+    if screenRect is not None:
+        minX = min(screenRect[1][0][0],screenRect[2][0][0])
+        maxX = min(screenRect[0][0][0],screenRect[3][0][0])
+        minY = max(screenRect[0][0][1],screenRect[1][0][1])
+        maxY = max(screenRect[2][0][1],screenRect[3][0][1])
 
-#         cropped_frame = frame[minX:maxX, minY:maxY]
-#         cv2.imshow('cropped', cropped_frame)
+        if maxX-minX>0 and maxY-minY>0 and sum_boundary_count<MEAN_BOUNDARY_SIZE:
+            if sum_boundary_count==0:
+                sum_boundary.append([minX,maxX,minY,maxY])
+            else:
+                sum_boundary = np.insert(sum_boundary,0,[minX,maxX,minY,maxY],axis=0)
+            sum_boundary_count = sum_boundary_count + 1
+            print (sum_boundary_count/MEAN_BOUNDARY_SIZE)*100," % complete..."
 
-    cv2.drawContours(frame, [screenRect], -1, (0,0,255), 3)
+    if sum_boundary_count>=MEAN_BOUNDARY_SIZE and mean_boundary is None:
+        mean_boundary = np.sum(sum_boundary,axis=0)/sum_boundary_count
+        print "calibration complete..."
 
-    cv2.imshow('video', frame)
+
+while True:
+    ret, frame = cap.read()
+
+    #########################
+    ## Calibration Routine ##
+    #########################
+
+    if mean_boundary is None:
+        calibrate_screen(frame)
+
+    ########################
+        
+    if mean_boundary is not None and len(mean_boundary)==4:
+        cropped_frame = frame[mean_boundary[2]:mean_boundary[3],mean_boundary[0]:mean_boundary[1]]
+        cv2.imshow('cropped', cropped_frame)
+
+    #cv2.drawContours(frame, [screenRect], -1, (0,0,255), 3)
+
+    #cv2.imshow('video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
             break
